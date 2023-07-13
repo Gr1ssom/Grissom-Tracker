d944e0566049c03ba03c5e5b318a7c570c3bffe6
Dylan Grissom <dgriss295@gmail.com>
2023-07-13 14:18:35 -0500

    forgot to push these weeks ago, thanks brain damage

---
+++ schema.sql
@@ -0,0 +1,25 @@
+-- Create department table
+CREATE TABLE department (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL
+);
+
+-- Create role table
+CREATE TABLE role (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+-- Create employee table
+CREATE TABLE employee (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role (id),
+  FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
